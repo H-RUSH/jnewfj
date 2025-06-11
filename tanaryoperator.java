5b934fdc18fd1d286b937383d51d6c7d1a58d424
Himadry Mondol <himadrymondol81@gmail.com>
2025-06-12 03:02:17 +0600

    hellow man

--- tanaryoperator.java
+++ tanaryoperator.java
@@ -12,5 +12,8 @@ public class tanaryoperator {
     int himadry=scanner.nextInt();
     String mondol=(himadry<18)?"1":"0";
     System.out.println(mondol);
+    int sapla=scanner.nextInt();
+    System.out.println((sapla<10)?"hellow its less then 10":"its more then 10");
+   scanner.close();
  }
 }
